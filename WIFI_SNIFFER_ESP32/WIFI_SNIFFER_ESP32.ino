f694ab7b76f4ece66f08f8109adf3531a0af2463
kn1cht <16277200+kn1cht@users.noreply.github.com>
2019-05-03 00:08:43 +0900

    create wifi_sniffer.cpp

--- WIFI_SNIFFER_ESP32/WIFI_SNIFFER_ESP32.ino
+++ WIFI_SNIFFER_ESP32/WIFI_SNIFFER_ESP32.ino
@@ -1,17 +1,22 @@
 #include <WiFi.h>
 #include "wifi_sniffer.hpp"
+#define	WIFI_CHANNEL_MAX		(13)
+#define	WIFI_CHANNEL_SWITCH_INTERVAL	(500)
 
 uint8_t channel = 1;
+
+
+WiFiSniffer* sniffer;
 
 void setup() {
   Serial.begin(115200);
   delay(10);
-  wifi_sniffer_init();
+	sniffer = new WiFiSniffer();
 }
 
 void loop() {
   delay(1000);
   vTaskDelay(WIFI_CHANNEL_SWITCH_INTERVAL / portTICK_PERIOD_MS);
-  wifi_sniffer_set_channel(channel);
+  sniffer->set_channel(channel);
   channel = (channel % WIFI_CHANNEL_MAX) + 1;
 }
---
+++ WIFI_SNIFFER_ESP32/wifi_sniffer.cpp
@@ -0,0 +1,49 @@
+#include "wifi_sniffer.hpp"
+
+esp_err_t event_handler(void *ctx, system_event_t *event) { return ESP_OK; }
+
+WiFiSniffer::WiFiSniffer() {
+  tcpip_adapter_init();
+	ESP_ERROR_CHECK( esp_event_loop_init(event_handler, NULL) );
+  wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+	ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
+	ESP_ERROR_CHECK( esp_wifi_set_country(&wifi_country) );
+	ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
+  ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_NULL) );
+  ESP_ERROR_CHECK( esp_wifi_start() );
+	esp_wifi_set_promiscuous(true);
+	esp_wifi_set_promiscuous_rx_cb(&packet_handler);
+}
+
+const char* WiFiSniffer::packet_type_to_str(wifi_promiscuous_pkt_type_t type) {
+  switch(type) {
+    case WIFI_PKT_MGMT: return "MGMT";
+    case WIFI_PKT_DATA: return "DATA";
+    case WIFI_PKT_CTRL: return "DATA";
+    case WIFI_PKT_MISC: return "MISC";
+    default: return "????";
+  }
+}
+
+void WiFiSniffer::packet_handler(void* buff, wifi_promiscuous_pkt_type_t type) {
+  const wifi_promiscuous_pkt_t  *ppkt = (wifi_promiscuous_pkt_t *)buff;
+  const wifi_ieee80211_packet_t *ipkt = (wifi_ieee80211_packet_t *)ppkt->payload;
+  const wifi_ieee80211_mac_hdr_t *hdr = &ipkt->hdr;
+
+  printf(
+    "PACKET TYPE=%s, CHAN=%02d, RSSI=%02d,"
+    " ADDR1=%02x:%02x:%02x:%02x:%02x:%02x,"
+    " ADDR2=%02x:%02x:%02x:%02x:%02x:%02x,"
+    " ADDR3=%02x:%02x:%02x:%02x:%02x:%02x\n",
+    packet_type_to_str(type),
+    ppkt->rx_ctrl.channel,
+    ppkt->rx_ctrl.rssi,
+    hdr->addr1[0],hdr->addr1[1],hdr->addr1[2],hdr->addr1[3],hdr->addr1[4],hdr->addr1[5],
+    hdr->addr2[0],hdr->addr2[1],hdr->addr2[2],hdr->addr2[3],hdr->addr2[4],hdr->addr2[5],
+    hdr->addr3[0],hdr->addr3[1],hdr->addr3[2],hdr->addr3[3],hdr->addr3[4],hdr->addr3[5]
+  );
+}
+
+void WiFiSniffer::set_channel(uint8_t channel) {
+  esp_wifi_set_channel(channel, WIFI_SECOND_CHAN_NONE);
+}
